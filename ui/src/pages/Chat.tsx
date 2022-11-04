966e6413d2441fc880aff464fd544db37adbf879
zakken <matheus@marcogabriel.com.br>
2022-11-04 16:06:10 -0300

    created some events

--- ui/src/pages/Chat.tsx
+++ ui/src/pages/Chat.tsx
@@ -6,13 +6,24 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons'
 function Chat() {
 
     const [isTyping, setIsTyping] = useState(true);
-    let utcTimer;
+    let utcTimer: any;
+    let fakeNum = 0;
 
     let messages = document.querySelector('.messages');
 
+    function val(el : any) {
+        if (el.options && el.multiple){
+            return el.options
+            .filter((option: any) => option.selected)
+            .map((option: any) => option.value)
+        } else {
+            return el.value;
+        }
+    }
+
     function userTypingClear() {
         return utcTimer = setTimeout(() => {
-            document.querySelector(".message.personal.typing").remove();
+            document.querySelector(".message.personal.typing")!.remove();
             setIsTyping(true);
         }, 3500)
     }
@@ -28,8 +39,46 @@ function Chat() {
     let fakeMsg = ["Hi there, I\'m Kelly and you?", "Nice to meet you", "How are you doing?", "Pretty good", "How\'s life been treating you?", "It could be worse, thanks", "I\'ve gotta go now", "It was a pleasure chat with you", "Bye :)"];
 
     function setFakeMessage() {
-        let typing
+        let typing: any = document.querySelector("div")!.insertAdjacentHTML("beforeend", "<span>")    
+        typing.add('message typing');
+        return setTimeout(() => {
+            typing.remove();
+            let msg: any = document.querySelector('div');
+            msg.add("message");
+            msg.textContent(fakeMsg[fakeNum]);
+            setDate();
+            return fakeNum++
+        }, 1000 + (Math.random() * 10) * 100);
     }
+
+    function insertMessage() {
+        let msgEl = document.querySelector(".action-box-input");
+        let msgText = val(msgEl);
+        if(msgText.trim() === "") {
+            return false;
+        }
+        let newMsg = document.querySelector("div")
+        newMsg.("personal")
+        setDate()
+        let actionBoxInput = document.querySelector(".action-box-input");
+        setIsTyping(true);
+        clearTimeout(utcTimer);
+
+        if (fakeMsg[fakeNum] === "") {
+            return false;
+        }
+        return setTimeout(() => {
+            return setFakeMessage();
+        }, 500 + (Math.random() * 10) * 100);
+    };
+
+    document.addEventListener("keydown", (ev) => {
+        if (ev.key.length === 13) {
+            insertMessage();
+            return false;
+        }
+    })
+
 
     return (
         <div className="chat">
@@ -44,10 +93,13 @@ function Chat() {
             </div>
             <div className="action-box">
                 <textarea className="action-box-input" placeholder="Type message..."></textarea>
-                <button className="action-box-submit" type="submit"><FontAwesomeIcon icon={faPaperPlane} /></button>
+                <button id="input" onClick={insertMessage} className="action-box-submit" type="submit"><FontAwesomeIcon icon={faPaperPlane} /></button>
             </div>
         </div>
     );
 }
+    
+const actionBoxInput = document.querySelector('#input');
+console.log(actionBoxInput);
 
 export default Chat;
