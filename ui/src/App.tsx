11eef29ff13d9b2555fa58bae3d0815f48d97c66
Matheus Zakken <matheus@marcogabriel.com.br>
2022-11-08 18:55:42 -0300

    Remake the App

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,67 @@
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import React, { SyntheticEvent, useEffect, useState } from 'react'
+import { Nav, NavLinks, StyledApp } from './styles'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter, Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+
+import { getFromLocalStorage, setToLocalStorage } from './helpers/storage'
+
+import { useTheme } from './themes/useTheme'
+import { GlobalStyles } from './themes/globalStyles'
+import { defaultThemes, ITheme } from './themes/themes'
+
+import Login from "./pages/Login"
 
 import Chat from './pages/Chat';
 
 function App() {
+    const loggedIn = getFromLocalStorage("login-state")
+    const navigate = useNavigate()
+
+    const localThemes: { [key: string]: ITheme } =
+        getFromLocalStorage("all-themes")
+    if (!localThemes) {
+        setToLocalStorage("all-themes", defaultThemes)
+    }
+
+    const { theme, themeLoaded } = useTheme()
+    const [selectedTheme, setSelectedTheme] = useState(theme)
+
+    useEffect(() => {
+        setSelectedTheme(theme)
+    }, [theme, themeLoaded]) 
+
+    const logoutHandler = (e: SyntheticEvent) => {
+        setToLocalStorage("login-state", false)
+        navigate("/", { replace: true })
+    }
+
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Chat />}></Route>
-            </Routes>
-        </BrowserRouter>
+        <>
+            {themeLoaded && (
+                <ThemeProvider theme={selectedTheme} >
+                    <GlobalStyles />
+                    <StyledApp>
+                        <header>
+                            <Nav>
+                                {loggedIn && ( 
+                                    <NavLinks onClick={logoutHandler}>Logout</NavLinks>
+                                )}
+                            </Nav>
+                        </header>
+                            <Routes>
+                                <Route 
+                                    path="/"
+                                    element={
+                                        loggedIn ? <Navigate to="/chat" /> : <Navigate to="/login" />
+                                    }
+                                />
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/chat" element={<Chat />} />
+                            </Routes>
+                    </StyledApp>
+                </ThemeProvider>
+            )}
+        </>
     );
 }
 
