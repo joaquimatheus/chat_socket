a07dd8627d055b1ff5acfc66b9c8e8c44f0e89d3
Matheus Zakken <matheus@marcogabriel.com.br>
2022-11-18 02:29:36 -0300

    created a service to manipulate the requests and events with rxjs

--- ui/src/services/socketService.ts
+++ ui/src/services/socketService.ts
@@ -1,19 +1,35 @@
-import * as io from 'socket.io-client'
+import { io } from 'socket.io-client'
+import { Observer, Observable } from 'rxjs'
+
+import { IChat } from '../components/ChatBubble'
+import { Event } from '../model/Event'
 
 const SERVER_URL = 'http://localhost:8080'
 
-export class SocketServic {
-    private socket: any;
+export class SocketService {
+    private socket:any = io(SERVER_URL)
 
     public initSocket(): void {
-        this.socket = io.connect(SERVER_URL);
+        if (this.socket.connected) {
+            this.socket.emit('connected!')
+        } else {
+            console.log(`not connected`)
+        }
     }
 
-    public send(message: any): void {
+    public send(message: IChat): void {
         this.socket.emit('message', message);
     }
 
-    public onMessage(): void {
-        return this.socket.on('message', )
+    public onMessage(): Observable<IChat> {
+        return new Observable<IChat>(observer => {
+            this.socket.on('message', (data: IChat) => observer.next(data))
+        });
+    }
+
+    public onEvent(event: Event): Observable<any> {
+        return new Observable<Event>(observer => {
+            this.socket.on(event, () => observer.next())
+        });
     }
 }
