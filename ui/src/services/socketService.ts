675ef684634444afa0ff6638904c73d1c357c6c4
Matheus Zakken <matheus@marcogabriel.com.br>
2022-11-23 14:18:41 -0300

    Created the Context API to only 1 time

--- ui/src/services/socketService.ts
+++ ui/src/services/socketService.ts
@@ -1,33 +1,5 @@
-import { io } from 'socket.io-client'
-import { Observer, Observable } from 'rxjs'
+import React, { createContext } from 'react';
+import io from 'socket.io-client';
 
-import { IChat } from '../components/ChatBubble'
-import { Event } from '../model/Event'
-
-const SERVER_URL = 'http://localhost:8080'
-
-export class SocketService {
-    private socket:any = io(SERVER_URL)
-
-    public initSocket(): void {
-        this.socket.on("connect", () => {
-            console.log(this.socket.id)
-        })
-    }
-
-    public send(message: IChat): void {
-        this.socket.emit('message', message);
-    }
-
-    public onMessage(): Observable<IChat> {
-        return new Observable<IChat>(observer => {
-            this.socket.on('message', (data: IChat) => observer.next(data))
-        });
-    }
-
-    public onEvent(event: Event): Observable<any> {
-        return new Observable<Event>(observer => {
-            this.socket.on(event, () => observer.next())
-        });
-    }
-}
+export const socket = io('http://localhost:8080');
+export const SocketContext = createContext(socket);
